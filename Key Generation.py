798834934d7a42bb23af898e43d31bb5c635acf6
Adam <adamayad@outlook.com>
2025-12-03 20:10:28 +0200

    fixed files

--- Key Generation.py
+++ Key Generation.py
@@ -1,16 +1,18 @@
-Permutation_1 = [57,49,41,33,25,17,9,1,58,50,42,34,26,18,10,2,59,51,43,35,27,19,11,3,60,52,44,36,63,55,47,39,31,23,15,7,62,54,46,38,30,22,14,6,61,53,45,37,29,21,13,5,28,20,12,4]
+perm_1 = [57,49,41,33,25,17,9,1,58,50,42,34,26,18,10,2,59,51,43,35,27,19,11,3,60,52,44,36,63,55,47,39,31,23,15,7,62,54,46,38,30,22,14,6,61,53,45,37,29,21,13,5,28,20,12,4]
 
-Permutation_2 = [14,17,11,24,1,5,3,28,15,6,21,10,23,19,12,4,26,8,16,7,27,20,13,2,41,52,31,37,47,55,30,40,51,45,33,48,44,49,39,56,34,53,46,42,50,36,29,32]
+perm_2 = [14,17,11,24,1,5,3,28,15,6,21,10,23,19,12,4,26,8,16,7,27,20,13,2,41,52,31,37,47,55,30,40,51,45,33,48,44,49,39,56,34,53,46,42,50,36,29,32]
+
+shift_schedule = [1,1,2,2,2,2,2,2,1,2,2,2,2,2,2,1]
 
 
 import random
 
-
-def generate_random_64bit_key():
-    key = []                  # start with an empty list
-    for i in range(64):       # loop 64 times
-        bit = random.randint(0, 1)   # generate 0 or 1
-        key.append(bit)              # add the bit to the list
+#This function will generate 64 numbers either 0 or 1 and add them to an empty list
+def generate_key():
+    key = []                  
+    for i in range(64):       
+        bit = random.randint(0, 1)   
+        key.append(bit)              
     return key
 
 
@@ -22,6 +24,8 @@ def permute(block, table):
         result.append(block[index - 1])
     return result
 
-
+#this function shifts the bits that we have depending on the shift schedule of the round
+def left_shift(block, n):
+     return block[n:] + block[:n]
 
 
