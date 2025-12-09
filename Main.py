b216d0dc74dca4919b710d32816269a2b3bde691
Adam <adamayad@outlook.com>
2025-12-09 22:15:05 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,6 +2,8 @@ from KeyGeneration import generate_key, generate_subkeys
 from Encryption import encrypt_block
 from Decryption import decrypt_block
 
+
+#this function will convert any string into bits and store them into a list
 def text_to_bits(text):
     bits = []
     for char in text:
@@ -10,6 +12,7 @@ def text_to_bits(text):
             bits.append(int(b))
     return bits
 
+#this function will split all the bits into blocks of 64 bits for the DES program to run
 def split_into_blocks(bits):
     blocks = []
     for i in range(0, len(bits), 64):
@@ -19,6 +22,7 @@ def split_into_blocks(bits):
         blocks.append(block)
     return blocks
 
+#this function will convert our bits into hexadecimal for readability of ciphertext
 def bits_to_hex(bits):
     hex_string = ""
     for i in range(0, len(bits), 4):
@@ -28,6 +32,7 @@ def bits_to_hex(bits):
         hex_string += format(nibble_int, "x") 
     return hex_string
 
+#this function will convert our hexdecimal into bits again when decrypting
 def hex_to_bits(hex_string):
     bits = []
     for h in hex_string:
@@ -35,6 +40,23 @@ def hex_to_bits(hex_string):
         for bit in b:
             bits.append(int(bit))
     return bits
+
+#Here we start the main program and take an input from the user for the plaintext
+print("Enter your message:")
+message = input("--> ")
+
+#Generate random 64-bit key and all the subkeys
+key64 = generate_key()
+subkeys = generate_subkeys(key64)
+
+#display the key in hexadecimal  so its readable
+print("\nGenerated Key (HEX):")
+print(bits_to_hex(key64))
+
+#convert the message into bits and then split the bits into blocks of 64 bits
+message_bits = text_to_bits(message)
+blocks = split_into_blocks(message_bits)
+
 
 
 
