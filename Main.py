f88b8c590bc4962c599c231a8c8020696ea5b60a
Adam <adamayad@outlook.com>
2025-12-09 21:03:02 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -19,5 +19,22 @@ def split_into_blocks(bits):
         blocks.append(block)
     return blocks
 
+def bits_to_hex(bits):
+    hex_string = ""
+    for i in range(0, len(bits), 4):
+        nibble = bits[i:i+4]               
+        nibble_str = "".join(str(b) for b in nibble)  
+        nibble_int = int(nibble_str, 2)   
+        hex_string += format(nibble_int, "x") 
+    return hex_string
+
+def hex_to_bits(hex_string):
+    bits = []
+    for h in hex_string:
+        b = f"{int(h,16):04b}"
+        for bit in b:
+            bits.append(int(bit))
+    return bits
+
 
 
