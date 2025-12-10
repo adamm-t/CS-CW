d5e3e41b15942c4df33b42c0be6500baada95c86
Adam <adamayad@outlook.com>
2025-12-10 14:58:58 +0200

    FIXED ENCRYPTION

--- Encryption.py
+++ Encryption.py
@@ -1,153 +1,173 @@
 #Initial permutation
-IP = [58,50,42,34,26,18,10,2,60,52,44,36,28,20,12,4,62,54,46,38,30,22,14,6,64,56,48,40,32,24,16,8,57,49,41,33,25,17,9,1,59,51,43,35,27,19,11,3,61,53,45,37,29,21,13,5,63,55,47,39,31,23,15,7]
-
-# Inverse initial permutation 
-IP_INV = [40,8,48,16,56,24,64,32,39,7,47,15,55,23,63,31,38,6,46,14,54,22,62,30,37,5,45,13,53,21,61,29,36,4,44,12,52,20,60,28,35,3,43,11,51,19,59,27,34,2,42,10,50,18,58,26,33,1,41,9,49,17,57,25]
-
-# Expansion table 
-E_table = [32,1,2,3,4,5,4,5,6,7,8,9,8,9,10,11,12,13,12,13,14,15,16,17,16,17,18,19,20,21,20,21,22,23,24,25,24,25,26,27,28,29,28,29,0,31,32,1]
-
-# S-boxes 
-S_BOXES = [
-# S1
-[
-[14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7],
-[0,15,7,4,14,2,13,1,10,6,12,11,9,5,3,8],
-[4,1,14,8,13,6,2,11,15,12,9,7,3,10,5,0],
-[15,12,8,2,4,9,1,7,5,11,3,14,10,0,6,13]
-],
-# S2
-[
-[15,1,8,14,6,11,3,4,9,7,2,13,12,0,5,10],
-[3,13,4,7,15,2,8,14,12,0,1,10,6,9,11,5],
-[0,14,7,11,10,4,13,1,5,8,12,6,9,3,2,15],
-[13,8,10,1,3,15,4,2,11,6,7,12,0,5,14,9]
-],
-# S3
-[
-[10,0,9,14,6,3,15,5,1,13,12,7,11,4,2,8],
-[13,7,0,9,3,4,6,10,2,8,5,14,12,11,15,1],
-[13,6,4,9,8,15,3,0,11,1,2,12,5,10,14,7],
-[1,10,13,0,6,9,8,7,4,15,14,3,11,5,2,12]
-],
-# S4
-[
-[7,13,14,3,0,6,9,10,1,2,8,5,11,12,4,15],
-[13,8,11,5,6,15,0,3,4,7,2,12,1,10,14,9],
-[10,6,9,0,12,11,7,13,15,1,3,14,5,2,8,4],
-[3,15,0,6,10,1,13,8,9,4,5,11,12,7,2,14]
-],
-# S5
-[
-[2,12,4,1,7,10,11,6,8,5,3,15,13,0,14,9],
-[14,11,2,12,4,7,13,1,5,0,15,10,3,9,8,6],
-[4,2,1,11,10,13,7,8,15,9,12,5,6,3,0,14],
-[11,8,12,7,1,14,2,13,6,15,0,9,10,4,5,3]
-],
-# S6
-[
-[12,1,10,15,9,2,6,8,0,13,3,4,14,7,5,11],
-[10,15,4,2,7,12,9,5,6,1,13,14,0,11,3,8],
-[9,14,15,5,2,8,12,3,7,0,4,10,1,13,11,6],
-[4,3,2,12,9,5,15,10,11,14,1,7,6,0,8,13]
-],
-# S7
-[
-[4,11,2,14,15,0,8,13,3,12,9,7,5,10,6,1],
-[13,0,11,7,4,9,1,10,14,3,5,12,2,15,8,6],
-[1,4,11,13,12,3,7,14,10,15,6,8,0,5,9,2],
-[6,11,13,8,1,4,10,7,9,5,0,15,14,2,3,12]
-],
-# S8
-[
-[13,2,8,4,6,15,11,1,10,9,3,14,5,0,12,7],
-[1,15,13,8,10,3,7,4,12,5,6,11,0,14,9,2],
-[7,11,4,1,9,12,14,2,0,6,10,13,15,3,5,8],
-[2,1,14,7,4,10,8,13,15,12,9,0,3,5,6,11]
+IP = [
+    58,50,42,34,26,18,10,2,
+    60,52,44,36,28,20,12,4,
+    62,54,46,38,30,22,14,6,
+    64,56,48,40,32,24,16,8,
+    57,49,41,33,25,17,9,1,
+    59,51,43,35,27,19,11,3,
+    61,53,45,37,29,21,13,5,
+    63,55,47,39,31,23,15,7
 ]
+
+#Inverse Initial Permutation
+IP_INV = [
+    40,8,48,16,56,24,64,32,
+    39,7,47,15,55,23,63,31,
+    38,6,46,14,54,22,62,30,
+    37,5,45,13,53,21,61,29,
+    36,4,44,12,52,20,60,28,
+    35,3,43,11,51,19,59,27,
+    34,2,42,10,50,18,58,26,
+    33,1,41,9,49,17,57,25
+]
+
+#Expansion table = 32 --> 48
+E = [
+    32,1,2,3,4,5,
+    4,5,6,7,8,9,
+    8,9,10,11,12,13,
+    12,13,14,15,16,17,
+    16,17,18,19,20,21,
+    20,21,22,23,24,25,
+    24,25,26,27,28,29,
+    28,29,30,31,32,1
+]
+
+#feistal permutation
+P = [
+    16,7,20,21,
+    29,12,28,17,
+    1,15,23,26,
+    5,18,31,10,
+    2,8,24,14,
+    32,27,3,9,
+    19,13,30,6,
+    22,11,4,25
+]
+
+#All the sboxes = 48 --> 32
+SBOX = [
+    # S1
+    [
+        [14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7],
+        [0,15,7,4,14,2,13,1,10,6,12,11,9,5,3,8],
+        [4,1,14,8,13,6,2,11,15,12,9,7,3,10,5,0],
+        [15,12,8,2,4,9,1,7,5,11,3,14,10,0,6,13]
+    ],
+    # S2
+    [
+        [15,1,8,14,6,11,3,4,9,7,2,13,12,0,5,10],
+        [3,13,4,7,15,2,8,14,12,0,1,10,6,9,11,5],
+        [0,14,7,11,10,4,13,1,5,8,12,6,9,3,2,15],
+        [13,8,10,1,3,15,4,2,11,6,7,12,0,5,14,9]
+    ],
+    # S3
+    [
+        [10,0,9,14,6,3,15,5,1,13,12,7,11,4,2,8],
+        [13,7,0,9,3,4,6,10,2,8,5,14,12,11,15,1],
+        [13,6,4,9,8,15,3,0,11,1,2,12,5,10,14,7],
+        [1,10,13,0,6,9,8,7,4,15,14,3,11,5,2,12]
+    ],
+    # S4
+    [
+        [7,13,14,3,0,6,9,10,1,2,8,5,11,12,4,15],
+        [13,8,11,5,6,15,0,3,4,7,2,12,1,10,14,9],
+        [10,6,9,0,12,11,7,13,15,1,3,14,5,2,8,4],
+        [3,15,0,6,10,1,13,8,9,4,5,11,12,7,2,14]
+    ],
+    # S5
+    [
+        [2,12,4,1,7,10,11,6,8,5,3,15,13,0,14,9],
+        [14,11,2,12,4,7,13,1,5,0,15,10,3,9,8,6],
+        [4,2,1,11,10,13,7,8,15,9,12,5,6,3,0,14],
+        [11,8,12,7,1,14,2,13,6,15,0,9,10,4,5,3]
+    ],
+    # S6
+    [
+        [12,1,10,15,9,2,6,8,0,13,3,4,14,7,5,11],
+        [10,15,4,2,7,12,9,5,6,1,13,14,0,11,3,8],
+        [9,14,15,5,2,8,12,3,7,0,4,10,1,13,11,6],
+        [4,3,2,12,9,5,15,10,11,14,1,7,6,0,8,13]
+    ],
+    # S7
+    [
+        [4,11,2,14,15,0,8,13,3,12,9,7,5,10,6,1],
+        [13,0,11,7,4,9,1,10,14,3,5,12,2,15,8,6],
+        [1,4,11,13,12,3,7,14,10,15,6,8,0,5,9,2],
+        [6,11,13,8,1,4,10,7,9,5,0,15,14,2,3,12]
+    ],
+    # S8
+    [
+        [13,2,8,4,6,15,11,1,10,9,3,14,5,0,12,7],
+        [1,15,13,8,10,3,7,4,12,5,6,11,0,14,9,2],
+        [7,11,4,1,9,12,14,2,0,6,10,13,15,3,5,8],
+        [2,1,14,7,4,10,8,13,15,12,9,0,3,5,6,11]
+    ]
 ]
 
 from KeyGeneration import permute
 
-#Encryption permutation
-rounds_p = [16,7,20,21,29,12,28,17,1,15,23,26,5,18,31,10,2,8,24,14,32,27,3,9,19,13,30,6,22,11,4,25]
+#this function will xor 2 things together and produce a string of bits
+def xor(a, b):
+    result = ""
 
-#this function here just does XOR on both halves of the bits 
-def xor_bits(a, b):
-    result = []
     for i in range(len(a)):
-        result.append(a[i] ^ b[i])
+        if a[i] != b[i]:
+            result += "1"
+        else:
+            result += "0"
+
     return result
 
-#this is the sbox function which reduces the bits from 48 to 32
-def sbox_substitution(block48):
-    output = []
+#this is our sbox substitution function which will reduce our bits from 48 to 32
+def sbox_substitution(bits48):
+    result = ""
 
-#this happens by first splitting the 48 bits into 8 groups of 6 bits 
-#each group of bits we take the row from the first and last bit and the column from the middle 4 bits
+    #we loop 8 times since there are 8 sboxes
     for i in range(8):
-        chunk = block48[i*6:(i+1)*6]
-        row = int(str(chunk[0]) + str(chunk[5]), 2)
-        col = ""
-        for b in chunk[1:5]:
-            col += str(b)
-        col = int(col, 2)
+        #each chunk will be 6 bits 
+        chunk = bits48[i*6:(i+1)*6]
+        #we take the row which is the first and last bit and then convert them to an integer
+        row = int(chunk[0] + chunk[5], 2)
+        #the column is the middle 4 bits then converted also
+        col = int(chunk[1:5], 2)
+        #the result is the number that is found in the sbox and then converted into 4 bits
+        result += f"{SBOX[i][row][col]:04b}"
+    return result
 
-#we lookup the value from the 48 bits using the row and col
-        value = S_BOXES[i][row][col]
-
-#convert the value to 4 bits and add it to a list which gets appended to the final output which will have 32 bits at the end
-    binary_string = f"{value:04b}"
-    bits4 = []
-    for char in binary_string:
-        bits4.append(int(char))
-   
-    output.extend(bits4)
-
-#this is feistal rounds function where we use the e table to expand the bits first so they are the same size as the key
-def rounds(r_side, subkey):
-    expanded = permute(r_side, E_table)
-
-#then we xor the expanded bits with key 
-    xored = xor_bits(expanded, subkey)
-
-#then we use the sbox function we made to reduce the xored bits again back to 32 
+#this is the feistal function which happens each round
+def feistel(right, subkey):
+    
+    #first we expand the right side with the e table
+    expanded = permute(right, E)
+    #then we xor the expanded bits with the key
+    xored = xor(expanded, subkey)
+    #then we use the sbox on the result to give us 32 bits again
     substituted = sbox_substitution(xored)
+    #lastly we do the feistal permutation on it to shuffle the bits
+    return permute(substituted, P)
 
-#lastly we use the feistal permutation on the bits 
-    return permute(substituted, rounds_p)
-
-#this is the actual encryption function which takes the plaintext bits and the subkeys list
+#this function is the full encrypting process
 def encrypt_block(block64, subkeys):
+    #first we do the initial permutation on the 64 bits(plaintext)
+    block = permute(block64, IP)
 
-#first we check if both inputs are valid before starting
-    if len(block64) != 64:
-        print("Error: plaintext must be 64 bits")
-        return
-    if len(subkeys) != 16:
-        print("Error: there must be 16 subkeys")
-        return 
+    #split the bits into left and right side
+    L = block[:32]
+    R = block[32:]
 
-#first step is to do the initial permutation on the plaintext using the IP that is predefined    
-    permuted = permute(block64, IP)
-
-#then we split the bits into a left and right side for the actual feistal rounds
-    left = permuted[:32]
-    right = permuted[32:]
-
-#here the 16 rounds start and we use the rounds function to do all the calculations
+    #loop 16 times since they are 16 keys
     for i in range(16):
-        f_out = rounds(right, subkeys[i])         
-        
-#then we make the new right half equal the old left half XORED with the result we got from the rounds function       
-        new_right = xor_bits(left, f_out)         
+        temp = R
+        #the new right side is the old left side XORED with the feistal function of the right side and the round key
+        R = xor(L, feistel(R, subkeys[i]))
+        #new left side is the old right side
+        L = temp
 
-#next step is to swap the halves 
-        left = right
-        right = new_right
+    #we swap the halves and combine them
+    swapped = R + L
+    #we get our ciphertext by applying the inverse initial permutation on the bits
+    ciphertext = permute(swapped, IP_INV)
+    return ciphertext
 
-#then we combine both halves so we have 64 bits again and we just do the inverse initial permutation on it which gives us the cipher text
-    preoutput = right + left                       
-    cipher = permute(preoutput, IP_INV)
-    return cipher
-  
